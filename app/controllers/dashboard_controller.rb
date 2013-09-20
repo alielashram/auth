6950bb531ac76fa4eab107bac764cd1006717ee4
Ali El-Ashram <ali.elashram@gmail.com>
2013-09-19 23:55:19 -0700

    Create authentication/authorization system.

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+
+    before_action :user_logged_in!
+
+	def static
+	end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:email].downcase
+    if user && user.authenticate(params[:password])
+    	session[:user_id] = user.id
+        flash[:notice] = "You are signed in!"
+    	redirect_to root_url
+    else
+        flash.now[:alert] = "Invaild email/password."
+    	render 'new'
+  end
+ end
+
+ def destroy
+   sign_out
+   flash[:notice] = "You are now signed out!"
+   redirect_to root_url
+ end
+end
